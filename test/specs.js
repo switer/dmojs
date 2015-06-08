fd2bc693e941fd95b4fac125766386b6bd3960e2
switer <switer@switerdeMacBook-Pro.local>
2015-06-08 17:52:40 +0800

    init test case

--- test/specs.js
+++ test/specs.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function (DMO, assert) {
-	
+	describe('#instance', function () {
+		it('Instance with "props" option', function () {
+			var d = new DMO({
+				props: {
+					title: 'dmo'
+				}
+			})
+			assert.equal(d.$data.title, 'dmo')
+		})
+	})
+	describe('#get', function () {
+		it('get data', function () {
+			var d = new DMO({
+				props: {
+					title: 'dmo',
+					posts: [{
+						head: 'data'
+					}]
+				}
+			})
+			assert.equal(d.$get('title'), 'dmo')
+			assert.equal(d.$get('posts[0].head'), 'data')
+		})
+	})
 }
